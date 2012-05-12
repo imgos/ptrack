085a9a6b7a254a83f50c0f589a92426c435a74e9
Brett Maden <os@maden.com>
2012-05-12 17:32:32 -0600

    load a map

--- src/ptrackUi/MapWidget.cpp
+++ src/ptrackUi/MapWidget.cpp
@@ -2,13 +2,76 @@
 
 namespace ptui {
 
+const char* MapWidget::kHtml = "<html><head><script type=\"text/javascript\" " \
+    "src=\"http://maps.google.com/maps/api/js?sensor=false\"></script>" \
+    "<script  type=\"text/javascript\">" \
+    "var map; function init(lat, lng) { "\
+    "map = new google.maps.Map(document.getElementById(\"map_canvas\"), " \
+    "{ zoom: 5, center: new google.maps.LatLng(lat, lng), " \
+    "disableDefaultUI: true, mapTypeId: google.maps.MapTypeId.ROADMAP  });" \
+    "} </script>" \
+    "</head><body style=\"margin:0px; padding:0px;\">" \
+    "<div id=\"map_canvas\" style=\"width:100%; height:100%\"></div>" \
+    "</body></html>";
+
 /*
  * MapWidget
  */
 MapWidget::MapWidget( QWidget* parent )
- : QWebView( parent )
+ : QWebView( parent ),
+   mNetManager( new QNetworkAccessManager( this ) )
 {
+  QWebFrame* frame = page()->mainFrame();
+  frame->setHtml( QString( kHtml ) );
 
+  QTimer::singleShot( 1000, this, SLOT( triggerLoading() ) );
+}
+
+/*
+ * triggerLoading
+ */
+void MapWidget::triggerLoading()
+{
+  QString code = "init(37.6970,  -91.8096)";
+  QWebFrame *frame = page()->mainFrame();
+  frame->evaluateJavaScript( code );
+}
+
+/*
+ * mousePressEvent
+ */
+void MapWidget::mousePressEvent( QMouseEvent* event )
+{
+  mPressed = true;
+  mPressPos = event->pos();
+  mDragPos = event->pos();
+  QWebView::mousePressEvent( event );
+}
+
+/*
+ * mouseReleaseEvent
+ */
+void MapWidget::mouseReleaseEvent( QMouseEvent* event )
+{
+  mPressed = false;
+  event->accept();
+  update();
+
+  QWebView::mouseReleaseEvent( event );
+}
+
+/*
+ * mouseMoveEvent
+ */
+void MapWidget::mouseMoveEvent( QMouseEvent* event )
+{
+  if( !mPressed ) {
+    QWebView::mouseMoveEvent( event );
+    return;
+  }
+
+  mDragPos = event->pos();
+  update();
 }
 
 } // namespace ptui
--- src/ptrackUi/MapWidget.h
+++ src/ptrackUi/MapWidget.h
@@ -3,7 +3,10 @@
 
 #include <ptrackUi/PTrackUiMacros.h>
 
+#include <QtNetwork/QNetworkAccessManager>
 #include <QtWebKit/QtWebKit>
+
+#include <boost/shared_ptr.hpp>
 
 namespace ptui {
 
@@ -14,6 +17,22 @@ class PTUI_API MapWidget : public QWebView
 public:
   MapWidget( QWidget* parent = 0 );
 
+protected:
+  void mousePressEvent( QMouseEvent* event );
+  void mouseReleaseEvent( QMouseEvent* event );
+  void mouseMoveEvent( QMouseEvent* event );
+
+private Q_SLOTS:
+  void triggerLoading();
+
+private:
+  boost::shared_ptr< QNetworkAccessManager > mNetManager;
+
+  bool mPressed;
+  QPoint mPressPos;
+  QPoint mDragPos;
+
+  static const char* kHtml;
 };
 
 } // namespace ptui
