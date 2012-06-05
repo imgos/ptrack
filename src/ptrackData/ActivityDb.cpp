8aee1157cd2157f8ca26b2b229172d323815112c
Brett Maden <os@maden.com>
2012-06-05 14:36:13 -0600

    added activity storage and database update functions

--- src/ptrackData/ActivityDb.cpp
+++ src/ptrackData/ActivityDb.cpp
@@ -24,9 +24,8 @@ ActivityDb::ActivityDb( const std::string& fileName )
  */
 ActivityDb::~ActivityDb()
 {
-  if( mDb != NULL ) {
-    sqlite3_close( mDb );
-  }
+  // calling close on a null db is okay
+  sqlite3_close( mDb );
 }
 
 /*
@@ -122,6 +121,68 @@ void ActivityDb::categoryNameQuery( const std::string& categoryName )
       break;
     }
   }
+}
+
+/*
+ * updateActivity
+ */
+bool ActivityDb::updateActivity( const std::string& category,
+  const QDateTime& dateTime,
+  const std::string& gpsTrack,
+  const double& totalTime,
+  const double& totalDistance,
+  const int& uniqueId )
+{
+  return updateActivity( Activity( category, dateTime, gpsTrack, totalTime, totalDistance, uniqueId ) );
+}
+
+/*
+ * updateActivity
+ */
+bool ActivityDb::updateActivity( const Activity& activity )
+{
+  if( !status() ) {
+    return false;
+  }
+
+  sqlite3_stmt* statement;
+
+  const char* sql = "UPDATE activity SET " \
+                    "  category = ?, " \
+                    "  dateTime = ?, " \
+                    "  gpsTrack = ?, " \
+                    "  totalTime = ?, " \
+                    "  totalDistance = ? " \
+                    "WHERE rowid = ?";
+
+  int retVal = sqlite3_prepare_v2( mDb, sql, -1, &statement, NULL );
+  if( retVal != SQLITE_OK ) {
+    std::cerr << "sqlite prepare failed" << std::endl;
+    return false;
+  }
+
+  // safely bind data
+  sqlite3_bind_text( statement, 1, activity.category.c_str(), -1, SQLITE_TRANSIENT );
+  QString dateString = activity.dateTime.toString();
+  std::string dateStdString = dateString.toStdString();
+  sqlite3_bind_text( statement, 2, dateStdString.c_str(), -1, SQLITE_TRANSIENT );
+  sqlite3_bind_text( statement, 3, activity.gpsTrack.c_str(), -1, SQLITE_TRANSIENT );
+  sqlite3_bind_double( statement, 4, activity.totalTime );
+  sqlite3_bind_double( statement, 5, activity.totalDistance );
+  sqlite3_bind_int( statement, 6, activity.uniqueId );
+
+  // execute the update
+  if( sqlite3_step( statement ) != SQLITE_OK ) {
+    std::cerr << "database update failed" << std::cerr;
+    return false;
+  }
+
+  if( sqlite3_finalize( statement ) != SQLITE_OK ) {
+    std::cerr << "sqlite finalize failed" << std::cerr;
+    return false;
+  }
+
+  return true;
 }
 
 } // namespace ptdata
--- src/ptrackData/ActivityDb.h
+++ src/ptrackData/ActivityDb.h
@@ -3,6 +3,8 @@
 
 #include <ptrackData/PTrackDataMacros.h>
 
+#include <QDateTime>
+
 #include <string>
 
 struct sqlite3;
@@ -12,6 +14,35 @@ namespace ptdata {
 class PTDATA_API ActivityDb
 {
 public:
+
+  struct Activity {
+    Activity( const std::string& iCategory,
+      const QDateTime& iDateTime,
+      const std::string& iGpsTrack,
+      const double& iTotalTime,
+      const double& iTotalDistance,
+      const int& iUniqueId = -1 )
+     : category( iCategory ),
+       dateTime( iDateTime ),
+       gpsTrack( iGpsTrack ),
+       totalTime( iTotalTime ),
+       totalDistance( iTotalDistance ),
+       uniqueId( iUniqueId )
+    {}
+
+    Activity()
+     : uniqueId( -1 )
+    {}
+
+    int uniqueId;
+    std::string category;
+    QDateTime dateTime;
+    std::string gpsTrack;
+    double totalTime;
+    double totalDistance;
+  };
+
+
   ActivityDb( const std::string& fileName );
   ~ActivityDb();
 
@@ -20,6 +51,14 @@ public:
   bool status() const;
 
   void categoryNameQuery( const std::string& categoryName );
+
+  bool updateActivity( const std::string& category,
+    const QDateTime& dateTime,
+    const std::string& gpsTrack,
+    const double& totalTime,
+    const double& totalDistance,
+    const int& uniqueId );
+  bool updateActivity( const Activity& activity );
 
 private:
   std::string mFileName;
