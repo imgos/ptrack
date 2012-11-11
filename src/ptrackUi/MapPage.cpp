f1f9dfed560ac0b242e4f391572abf9f9c0bd573
Brett Maden <os@maden.com>
2012-11-11 14:15:03 -0700

    use the user-agent from chrome 24 as dragging/zooming had stopped working with the other user-agent string

--- src/ptrackUi/MapPage.cpp
+++ src/ptrackUi/MapPage.cpp
@@ -4,7 +4,7 @@ namespace ptui {
 
 QString MapPage::userAgentForUrl( const QUrl& url ) const
 {
-  return "Chrome/1.0";
+  return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.5 Safari/537.17";
 }
 
 }
